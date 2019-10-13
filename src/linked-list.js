cd08e5169278a49c194220ac9c58ee117429b160
Vital Yurau <vialyur@list.ru>
2019-10-13 22:43:35 +0300

    docs: start

--- src/linked-list.js
+++ src/linked-list.js
@@ -1,27 +1,108 @@
 const Node = require('./node');
 
-class LinkedList {
-    constructor() {}
+class LinkedList {  
+    constructor() {
+        this._head = null;
+        this._tail = null;
+        this._length = 0;
+    }
 
-    append(data) {}
+    get length() {
+        return this._length;
+    }
 
-    head() {}
+    append(data) {
+        let node = new Node(data);
 
-    tail() {}
+        if(this._length ===  0) {
+            this._head = node;
+            this._tail = node;
+        } else {
+            this._tail.next = node;
+            node.prev = this._tail;
+            this._tail = node;
+        }
 
-    at(index) {}
+        this._length += 1;
+  
+        return this;
+    }
 
-    insertAt(index, data) {}
+    head() {
+        return (!this._head) ? null : this._head.data;
+    }
 
-    isEmpty() {}
+    tail() {
+        return (!this._tail) ? null : this._tail.data;
+    }
 
-    clear() {}
+    _at(index) {
+        let node = this._head;
+        for(let i = index; i > 0; i--){
+            node = node.next;
+        }
+        return node;
+    }
 
-    deleteAt(index) {}
+    at(index) {
+        return this._at(index).data;
+    }
 
-    reverse() {}
+    insertAt(index, data) {
+        
+        let addElem = new Node(data);
+        let nextElem = this._at(index);
+        let prevElem = nextElem.prev;
+
+        addElem.next = nextElem;
+        addElem.prev = prevElem;
+
+        prevElem.next = addElem;
+        nextElem.prev = addElem;
+        
+        this._length += 1;
+
+        return this;
+    } 
+
+    isEmpty() {
+        return (!this._length);
+    }
+
+    clear() {
+        this._head = null;
+        this._tail = null;
+        this._length = 0;
+        
+        return this;
+    }
+
+    deleteAt(index) {
+        let deleteElem = this._at(index);
+        let prevElem = deleteElem.prev;
+        let nextElem = deleteElem.next;
+
+        prevElem.next = nextElem;
+        nextElem.prev = prevElem;
+
+        this._length -= 1;
+
+        return this;
+    }
+
+    reverse() {
+        let tmp = this._head;
+        this._head = this._tail;
+        this._tail = tmp;
+        
+        let current 
+
+        return this;
+    }
 
     indexOf(data) {}
 }
+
+console.log(LinkedList.tail)
 
 module.exports = LinkedList;
